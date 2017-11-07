5a87e7b78c11bdb35b888a8b388611c39430e4ac
Humphrey Hu <humhu@cmu.edu>
2017-11-07 16:39:22 -0500

    Commented out printouts

--- include/graphopt/GraphOptimizer.h
+++ include/graphopt/GraphOptimizer.h
@@ -23,28 +23,28 @@ public:
 	template <typename N>
 	void AddNode( const std::shared_ptr<N>& node )
 	{
-		ROS_INFO_STREAM( "Adding node to graph: " << node.get() );
+	  //ROS_INFO_STREAM( "Adding node to graph: " << node.get() );
 		_optimizer->add_node( node.get() );
 	}
 
 	template <typename F>
 	void AddFactor( const std::shared_ptr<F>& factor )
 	{
-		ROS_INFO_STREAM( "Adding factor to graph: " << factor.get() );		
+	  //ROS_INFO_STREAM( "Adding factor to graph: " << factor.get() );		
 		_optimizer->add_factor( factor.get() );
 	}
 
 	template <typename N>
 	void RemoveNode( const std::shared_ptr<N>& node )
 	{
-		ROS_INFO_STREAM( "Removing node from graph: " << node.get() );
+	  //ROS_INFO_STREAM( "Removing node from graph: " << node.get() );
 		_optimizer->remove_node( node.get() );
 	}
 
 	template <typename F>
 	void RemoveFactor( const std::shared_ptr<F>& factor )
 	{
-		ROS_INFO_STREAM( "Removing factor from graph: " << factor.get() );
+	  //ROS_INFO_STREAM( "Removing factor from graph: " << factor.get() );
 		_optimizer->remove_factor( factor.get() );
 	}
 
